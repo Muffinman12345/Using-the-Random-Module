dca0aecdf47456ea7050925f63f373cbc324e76a
ddfunk2 <77017235+ddfunk2@users.noreply.github.com>
2021-01-31 10:00:12 -0500

    Copy and paste

---
+++ random.py
@@ -0,0 +1,9 @@
+#Import the random module (REQUIRED)
+
+import random
+
+#Make a random variable
+
+myRandomVariable = random.randint(1, 100)
+
+#1 is the smallest possible number. The 100 is the largest possible number. This will result in making a variable that is a random number from 1 through 100
