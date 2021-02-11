ed1422a7493dbd0d77e429d9bae3b50a9636b394
ddfunk2 <77017235+ddfunk2@users.noreply.github.com>
2021-02-11 10:30:40 -0500

    Create random2.py

---
+++ random2.py
@@ -0,0 +1,7 @@
+#Import modules
+import random
+
+#Create random variable
+myVariable = random.random(1, 100)
+#This return a random number (like 3.1415) from 1 through 100
+#Check out the other module to learn to just get int's (numbers with no decimals)
